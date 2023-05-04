c3d543287f05676f4dfa2c210da3b8cb31adb247
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-05-04 16:18:27 -0400

    up

--- exer-03.c
+++ exer-03.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 void main ()
+/*Construa um programa que imprima a tabela de equivalência de graus Fahrenheit para
+centígrados. Os limites são de 50 a 70 graus Fahrenheit com intervalo de 1 grau.
+Fórmula: C = 5/9 *(F -32)*/
 {
-    int graus, i;
-
+    int i;
+    float celcius;
     for (i=50; i<=70;i++){
-
+        celcius = (5.0 / 9.0) * (i - 32.0);
+        printf("%f\n", celcius);
     }
-
-
 }
---
+++ exer-04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main ()
+/*Dado N inteiro positivo, dizemos que N é perfeito se for igual a soma de seus divisores
+positivos diferentes de N.
+Exemplo: 6 é perfeito, pois 1 + 2 + 3 = 6.
+Verificar se um dado número inteiro positivo é perfeito.*/
+{
+    int n, i;
+
+    printf ("Informe um numero inteiro positivo: ");
+    scanf("%d", &n);
+
+    for (i = 1;){
+
+    }
+
+}
